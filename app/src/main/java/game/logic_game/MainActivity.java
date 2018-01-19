0918215e75330b321d843ff5455988cd9b4dbf3f
Johannes(raxxor95) <johannes9533@hotmail.com>
2018-01-19 19:58:58 +0100

    Added a listener to button

--- app/src/main/java/game/logic_game/MainActivity.java
+++ app/src/main/java/game/logic_game/MainActivity.java
@@ -1,13 +1,31 @@
 package game.logic_game;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Add listener
+        Button start_button = (Button) findViewById(R.id.start_button); //grab a view and convert it to a button class
+        start_button.setOnClickListener(this); //this indicates that the onClick will be called
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.start_button:
+                //start new intent for example
+                Log.d("test123","test"); //test to check if the button is getting clicked ( printf test)
+                break;
+        }
     }
 }
