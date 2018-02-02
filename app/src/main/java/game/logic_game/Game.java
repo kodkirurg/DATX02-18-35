d2014b850376de6eaf688467e79a8b4f308b238a
Johannes(raxxor95) <johannes9533@hotmail.com>
2018-02-02 21:50:49 +0100

    Moving the 2 blue cards around

--- app/src/main/java/game/logic_game/Game.java
+++ app/src/main/java/game/logic_game/Game.java
@@ -2,12 +2,29 @@ package game.logic_game;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class Game extends AppCompatActivity {
+    private ViewGroup root;
+    private int _xDelta;
+    private int _yDelta;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+
+        //add listeners
+        CardView card_1 = (CardView) findViewById(R.id.card_1);
+        CardView card_2 = (CardView) findViewById(R.id.card_2);
+        card_1.setOnTouchListener(new TouchListener());
+        card_2.setOnTouchListener(new TouchListener());
     }
+
 }
