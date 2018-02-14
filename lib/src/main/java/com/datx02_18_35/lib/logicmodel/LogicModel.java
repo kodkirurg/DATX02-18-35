44007be2c447f282b718f1c5757e5e7aba6cf1ea
Robin Åstedt <robin.astedt@gmail.com>
2018-02-14 13:42:15 +0100

    Some changes to test-code in LogicModel and changed language level for comply

--- lib/src/main/java/com/datx02_18_35/lib/logicmodel/LogicModel.java
+++ lib/src/main/java/com/datx02_18_35/lib/logicmodel/LogicModel.java
@@ -4,6 +4,7 @@ import com.datx02_18_35.lib.logicmodel.expression.Expression;
 import com.datx02_18_35.lib.logicmodel.expression.ExpressionFactory;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class LogicModel {
     public static void main(String[] args) {
@@ -26,8 +27,12 @@ public class LogicModel {
 
         System.out.println(p1.equals(p2));
         System.out.println(p1.equals(q2));
-        ArrayList<ExpressionFactory.RuleType> list = (ArrayList<ExpressionFactory.RuleType>)exprFact.checkLegalRules(c1);
-        System.out.println(list.get(0)+""+ list.get(1));
+        Collection<ExpressionFactory.RuleType> col = exprFact.checkLegalRules(c1);
+        //col.forEach((type) -> System.out.println(type));
+        for (ExpressionFactory.RuleType type : col) {
+            System.out.println(type);
+        }
+        //System.out.println(list.get(0)+""+ list.get(1));
 	    System.out.println(c1.equals(c3));
         System.out.println(c1.logicEquals(c3));
 
