093bfa48c21a0762a0bbcdb92aff553d1b020efa
raxxor <johannes9533@hotmail.com>
2018-02-15 16:22:17 +0100

    Listener and simpe menu in toolbar

--- app/src/main/java/com/datx02_18_35/android/Game.java
+++ app/src/main/java/com/datx02_18_35/android/Game.java
@@ -2,10 +2,13 @@ package com.datx02_18_35.android;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 
 import game.logic_game.R;
@@ -34,5 +37,23 @@ public class Game extends AppCompatActivity  {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.toolbar, menu); //add more items under dir menu -> toolbar
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menu) {
+        Intent i = null;
+        switch(menu.getItemId()){
+            case R.id.item_assumption:
+                i = new Intent(this,Game.class); //change to scope/assumption classs
+                break;
+        }
+        startActivity(i);
+        return false;
+    }
 
 }
