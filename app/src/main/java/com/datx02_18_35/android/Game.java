cf9fce6bf659cdda4e15faf6a6772d0d681d8a6a
raxxor <johannes9533@hotmail.com>
2018-02-15 15:49:22 +0100

    topbar added

--- app/src/main/java/com/datx02_18_35/android/Game.java
+++ app/src/main/java/com/datx02_18_35/android/Game.java
@@ -1,23 +1,18 @@
 package com.datx02_18_35.android;
 
-import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
 
-import java.util.ArrayList;
 
 import game.logic_game.R;
 
 public class Game extends AppCompatActivity  {
+
+    Toolbar toolbar;
 
 
     @Override
@@ -31,6 +26,13 @@ public class Game extends AppCompatActivity  {
         ft.replace(R.id.game_right_side, new Fragment_board_actions());
         ft.replace(R.id.game_left_side , new Fragment_board_cards()).commit();
 
+
+        //Set toolbar
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        this.setSupportActionBar(toolbar);
+
     }
+
 
 }
