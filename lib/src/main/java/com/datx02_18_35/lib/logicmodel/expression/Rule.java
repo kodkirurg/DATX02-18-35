f4e122bb53e785724303035f62d56b253ae2fce2
Johan <ronnas@student.chalmers.se>
2018-02-15 12:45:15 +0100

    Skrev om getLegalRules metoden för att göra den lite mer lättläslig. La till en hjälpmetod createDisjunctionEliminationRule för att minska koddupliceringen när vi kollar om disjunction elimination är möjlig

--- lib/src/main/java/com/datx02_18_35/lib/logicmodel/expression/Rule.java
+++ lib/src/main/java/com/datx02_18_35/lib/logicmodel/expression/Rule.java
@@ -25,81 +25,84 @@ public class Rule {
     public static Collection<Rule> getLegalRules(Collection<Expression> expressions){
         ArrayList<Expression> exprs = (ArrayList<Expression>) expressions;
         ArrayList<Rule> legalRules = new ArrayList<Rule>();
-        switch(exprs.size()){
+        switch(exprs.size()) {
             case 1:
-
                 legalRules.add(new Rule(RuleType.DISJUNCTION_INTRODUCTION, exprs));
-                if(exprs.get(0) instanceof Conjunction){
+                if (exprs.get(0) instanceof Conjunction) {
                     legalRules.add(new Rule(RuleType.CONJUNCTION_ELIMINATION, exprs));
                 }
-                return legalRules;
             case 2:
                 ArrayList<Expression> reverseExprs = exprs;
                 Collections.reverse(reverseExprs);
 
                 legalRules.add(new Rule(RuleType.CONJUNCTION_INTRODUCTION, exprs));
-                legalRules.add(new Rule(RuleType.CONJUNCTION_INTRODUCTION,reverseExprs ));
+                legalRules.add(new Rule(RuleType.CONJUNCTION_INTRODUCTION, reverseExprs));
 
-                if(exprs.get(0) instanceof Implication && ((Implication)exprs.get(0)).operand1.logicEquals(exprs.get(0))) {
-                    legalRules.add(new Rule(RuleType.IMPLICATION_ELIMINATION,exprs));
+                if (exprs.get(0) instanceof Implication && ((Implication) exprs.get(0)).operand1.logicEquals(exprs.get(0))) {
+                    legalRules.add(new Rule(RuleType.IMPLICATION_ELIMINATION, exprs));
 
-                }else if(exprs.get(1) instanceof Implication && ((Implication)exprs.get(1)).operand1.logicEquals(exprs.get(0))){
-                    legalRules.add(new Rule(RuleType.IMPLICATION_ELIMINATION,reverseExprs));
+                } else if (exprs.get(1) instanceof Implication && ((Implication) exprs.get(1)).operand1.logicEquals(exprs.get(0))) {
+                    legalRules.add(new Rule(RuleType.IMPLICATION_ELIMINATION, reverseExprs));
                 }
 
-                if(exprs.get(0) instanceof Assumption){
-                    legalRules.add(new Rule(RuleType.IMPLICATION_INTRODUCTION,exprs));
-                }else if(exprs.get(1) instanceof Assumption){
-                    legalRules.add(new Rule(RuleType.IMPLICATION_INTRODUCTION,reverseExprs));
+                if (exprs.get(0) instanceof Assumption) {
+                    legalRules.add(new Rule(RuleType.IMPLICATION_INTRODUCTION, exprs));
+                } else if (exprs.get(1) instanceof Assumption) {
+                    legalRules.add(new Rule(RuleType.IMPLICATION_INTRODUCTION, reverseExprs));
                 }
 
-                return  legalRules;
+
             case 3:
-                ArrayList<Expression> exprsOrder = new ArrayList<>();
-                if(exprs.get(0) instanceof Disjunction && exprs.get(1) instanceof Implication && exprs.get(2) instanceof Implication){
-                    Disjunction disj = (Disjunction)exprs.get(0);
-                    Implication impl1 = (Implication)exprs.get(1);
-                    Implication impl2 = (Implication)exprs.get(2);
-                    if(impl1.operand2.logicEquals(impl2.operand2)) {
-                        if (disj.operand1.logicEquals(impl1.operand1) && disj.operand2.logicEquals(impl2.operand1)) {
-                            exprsOrder = exprs;
-                        }else if(disj.operand2.logicEquals(impl1.operand1) && disj.operand1.logicEquals(impl2.operand1)){
-                            exprsOrder.add(disj);
-                            exprsOrder.add(impl2);
-                            exprsOrder.add(impl1);
-                        }
-                    }
-                }else if(exprs.get(0) instanceof Implication && exprs.get(1) instanceof Disjunction && exprs.get(2) instanceof Implication){
-                    Disjunction disj = (Disjunction)exprs.get(1);
-                    Implication impl1 = (Implication)exprs.get(0);
-                    Implication impl2 = (Implication)exprs.get(2);
-                    if(impl1.operand2.logicEquals(impl2.operand2)) {
-                        if (disj.operand1.logicEquals(impl1.operand1) && disj.operand2.logicEquals(impl2.operand1)) {
-                            exprsOrder = exprs;
-                        }else if(disj.operand2.logicEquals(impl1.operand1) && disj.operand1.logicEquals(impl2.operand1)){
-                            exprsOrder.add(disj);
-                            exprsOrder.add(impl2);
-                            exprsOrder.add(impl1);
-                        }
-                    }
-                }else if(exprs.get(0) instanceof Implication && exprs.get(1) instanceof Implication && exprs.get(2) instanceof Disjunction){
-                    Disjunction disj = (Disjunction)exprs.get(2);
-                    Implication impl1 = (Implication)exprs.get(0);
-                    Implication impl2 = (Implication)exprs.get(1);
-                    if(impl1.operand2.logicEquals(impl2.operand2)) {
-                        if (disj.operand1.logicEquals(impl1.operand1) && disj.operand2.logicEquals(impl2.operand1)) {
-                            exprsOrder = exprs;
-                        }else if(disj.operand2.logicEquals(impl1.operand1) && disj.operand1.logicEquals(impl2.operand1)){
-                            exprsOrder.add(disj);
-                            exprsOrder.add(impl2);
-                            exprsOrder.add(impl1);
-                        }
-                    }
+                Rule disjElimRule;
+
+                if (exprs.get(0) instanceof Disjunction && exprs.get(1) instanceof Implication && exprs.get(2) instanceof Implication) {
+                    // First (0) element is disjunction.
+                    disjElimRule = createDisjunctionElimination(((Disjunction) exprs.get(0)), (Implication) exprs.get(1), (Implication) exprs.get(2));
+                } else if (exprs.get(0) instanceof Implication && exprs.get(1) instanceof Disjunction && exprs.get(2) instanceof Implication) {
+                    // Second (1) element is disjunction.
+                    disjElimRule = createDisjunctionElimination(((Disjunction) exprs.get(1)), (Implication) exprs.get(0), (Implication) exprs.get(2));
+
+                } else if (exprs.get(0) instanceof Implication && exprs.get(1) instanceof Implication && exprs.get(2) instanceof Disjunction) {
+                    //Third (2) element is disjunction.
+                    disjElimRule = createDisjunctionElimination(((Disjunction) exprs.get(2)), (Implication) exprs.get(0), (Implication) exprs.get(1));
+                } else {
+                    break;
                 }
-                legalRules.add(new Rule(RuleType.DISJUNCTION_ELIMINATION,exprsOrder));
+                if (disjElimRule != null) {
+                    legalRules.add(disjElimRule);
+                }
+
             default:
                 throw new IllegalArgumentException("Too many arguments");
         }
+        return legalRules;
 
+    }
+
+    //Help function to create a DisjunctionElimination rule with the order of the expressions: A or B, A>C, B>C
+    private static Rule createDisjunctionElimination(Disjunction disj, Implication impl1, Implication impl2){
+        ArrayList<Expression> exprsOrder = new ArrayList<Expression>();
+        if(impl1.operand2.logicEquals(impl2.operand2)) {
+            if (disj.operand1.logicEquals(impl1.operand1) && disj.operand2.logicEquals(impl2.operand1)) {
+                //Already in the correct order.
+                exprsOrder.add(disj);
+                exprsOrder.add(impl1);
+                exprsOrder.add(impl2);
+
+            }else if(disj.operand2.logicEquals(impl1.operand1) && disj.operand1.logicEquals(impl2.operand1)){
+                //Change order of implications.
+                exprsOrder.add(disj);
+                exprsOrder.add(impl2);
+                exprsOrder.add(impl1);
+
+            }else{
+                // Expressions don't match. Return null.
+                return null;
+            }
+        }else{
+            //Expressions don't match. Return null.
+            return  null;
+        }
+        return new Rule(RuleType.DISJUNCTION_ELIMINATION,exprsOrder);
     }
 }
