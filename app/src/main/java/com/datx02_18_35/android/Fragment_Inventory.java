ef4aa0a4f68a8a1a1b796e3565fb1c596a5dedd4
Magnus Harryson <Magnus.Harryson@hotmail.com>
2018-02-21 11:27:26 +0100

    Some minor adjustments to scope and inventory

--- app/src/main/java/com/datx02_18_35/android/Fragment_Inventory.java
+++ app/src/main/java/com/datx02_18_35/android/Fragment_Inventory.java
@@ -1,26 +1,43 @@
 package com.datx02_18_35.android;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+import com.datx02_18_35.lib.logicmodel.Inventory;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
 import game.logic_game.R;
+
 
 /**
  * Created by Magnus on 2018-02-14.
  */
 
-public class Fragment_Inventory extends Fragment {
-public boolean open = false;
+public class Fragment_Inventory extends Fragment implements Serializable {
+    public boolean open = false;
+    private Fragment_board_cards cardBoard = new Fragment_board_cards();
+    private Inventory cards;
 
+    public void Fragment_Inventory(Inventory inven){
+        cards = inven;
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View frag = inflater.inflate(R.layout.inventory_frag, container, false);
-      //  RecyclerView rec = frag.findViewById(R.id.);
+
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft =fm.beginTransaction();
+
+        ft.replace(R.id.view_rec , cardBoard).commit();
 
 
 
@@ -35,4 +52,8 @@ public boolean open = false;
 
 
     }
+    public void updateInventory(){
+
+    }
+
 }
