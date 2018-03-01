b6f3dd4de9510cf63c5b59a0df0a8c0965a353db
Robin Åstedt <robin.astedt@gmail.com>
2018-03-01 16:54:35 +0100

    Implemented draft for message passing to and from Controller and View

--- controller/src/main/java/com/datx02_18_35/controller/Controller.java
+++ controller/src/main/java/com/datx02_18_35/controller/Controller.java
@@ -1,9 +1,22 @@
 package com.datx02_18_35.controller;
 
+import com.datx02_18_35.controller.actions.Action;
+import com.datx02_18_35.controller.actions.ActionConsumer;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 /**
  * Created by robin on 2018-03-01.
  */
 
-public class Controller {
+public class Controller extends ActionConsumer {
 
+    public Controller(ActionConsumer viewCallback) {
+        registerCallback(viewCallback);
+    }
+
+    @Override
+    public void handleAction(Action action) {
+
+    }
 }
