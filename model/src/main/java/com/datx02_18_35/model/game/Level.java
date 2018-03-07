68b04f5240c3276439f63ae69cd3d7e56afafea5
Jonatan Oberg <jonober@student.chalmers.se>
2018-03-07 15:46:05 +0100

    Added dummylevel for controller testing

--- model/src/main/java/com/datx02_18_35/model/game/Level.java
+++ model/src/main/java/com/datx02_18_35/model/game/Level.java
@@ -3,6 +3,7 @@ package com.datx02_18_35.model.game;
 import com.datx02_18_35.model.ExpressionParser;
 import com.datx02_18_35.model.expression.Expression;
 import com.datx02_18_35.model.expression.ExpressionFactory;
+import com.datx02_18_35.model.expression.OperatorType;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -10,6 +11,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -25,24 +27,89 @@ public class Level {
     private String levelName;
     private Expression goal;
     private Map<String,String> idToSymbol;
+    public ExpressionFactory expressionFactory;
 
+    public static final Level exampleLevel;
 
-
+    static {
+        Map<String,String> dummyMap = new HashMap<>();
+        ExpressionFactory expressionFactory = new ExpressionFactory(dummyMap);
+        Collection<Expression> hypothesis = new ArrayList<>();
+        Expression p = expressionFactory.createProposition("P");
+        Expression q = expressionFactory.createProposition("Q");
+        hypothesis.add(p);
+        hypothesis.add(q);
+        Expression goal = expressionFactory.createOperator(OperatorType.CONJUNCTION,p,q);
+        exampleLevel = new Level("dummy",hypothesis,goal,expressionFactory);
+    }
 
 
     public static Level createLevel(String filepath) throws NullPointerException, FileNotFoundException, IOException{
+        Map<String,String> map = new HashMap<>();
         Scanner input = new Scanner(new File(filepath));
+        List<String> lineList = new ArrayList<>();
         while(input.hasNextLine()){
+            String line = input.nextLine();
+            String[] strings =line.split(" ");
+            if(strings[0].equals("Symbol")){
+                if(strings.length!=3){
+                    throw new IllegalArgumentException("Text in file not formatted correctly");
+                }else{
+                    map.put(strings[1],strings[2]);
+                }
+
+            }else{
+                lineList.add(line);
+            }
 
         }
+        ExpressionFactory exprFactory = new ExpressionFactory(map);
+        ExpressionParser exprParser = new ExpressionParser(exprFactory);
+        Collection<Expression> hypo= new ArrayList<>();
+        Expression goal = null;
+        for(String string:lineList){
+            String[] strings=string.split(" ");
+            if(strings.length>0){
+                switch (strings[0]){
+                    case "Hypothesis":
+                        for(int i=1; i<strings.length;i++){
+                            hypo.add(exprParser.parseString(strings[i]));
+                        }
+                    case "goal":
+                        if(goal==null){
+                            if(strings.length==1) {
+                                goal = exprParser.parseString(strings[1]);
+                            }
+                        }
+
+                }
+            }
+        }
+
+
+        return null;
     }
-    public Level(String levelName,Collection<Expression> hypothesis, Expression goal, Map<String,String> idToSymbol){
+    private Level(String levelName,Collection<Expression> hypothesis, Expression goal,ExpressionFactory expressionFactory){
         this.hypothesis=hypothesis;
         this.goal=goal;
         this.idToSymbol=idToSymbol;
         this.levelName=levelName;
-        this.expressionFactory = new ExpressionFactory(idToSymbol);
-        this.expressionParser = new ExpressionParser(expressionFactory);
+        this.expressionFactory=expressionFactory;
+    }
 
+    private Level(){
+        Map<String,String> dummyMap= new HashMap<>();
+
+
+    }
+
+    public ExpressionFactory getExpressionFactory(){
+        return expressionFactory;
+    }
+
+    class InvalidParseFormatException extends Exception {
+        public InvalidParseFormatException(String string){
+            super();
+        }
     }
 }
