83044af050ec428d491d0666bb92b3b3ca8683b0
johannes(raxxor) <johannes9533@hotmail.com>
2018-03-07 14:44:11 +0100

    better support for bigger screens

--- app/src/main/java/com/datx02_18_35/android/FragmentBoardCards.java
+++ app/src/main/java/com/datx02_18_35/android/FragmentBoardCards.java
@@ -2,10 +2,14 @@ package com.datx02_18_35.android;
 
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,9 +37,13 @@ public class FragmentBoardCards extends Fragment implements OnStartDragListener 
         View view = inflater.inflate(R.layout.fragment_board_cards,
                 container, false);
 
+
+        int spanCount;
+        int widthDP=Math.round(getWidthDp(getActivity().getApplicationContext())) - 130*2;
+        for (spanCount=0; 130*spanCount < widthDP ;spanCount++);
         recyclerView = (RecyclerView) view.findViewById(R.id.game_recycler_view);
         // use a grid layout manager
-        recLayoutManager = new GridLayoutManager(getActivity(), 2);
+        recLayoutManager = new GridLayoutManager(getActivity(), spanCount);
         recyclerView.setLayoutManager(recLayoutManager);
 
         // specify an adapter (see also next example)
@@ -70,6 +78,17 @@ public class FragmentBoardCards extends Fragment implements OnStartDragListener 
 
         return view;
     }
+
+
+
+    public static float getWidthDp(Context context){
+        float px = Resources.getSystem().getDisplayMetrics().widthPixels;
+        Resources resources = context.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        float dp = px / ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        return dp;
+    }
+
 
     @Override
     public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
