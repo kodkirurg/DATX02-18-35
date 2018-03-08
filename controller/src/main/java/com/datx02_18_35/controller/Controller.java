f7bf031812f72c677691ccee3105df07f20bae57
Robin Åstedt <robin.astedt@gmail.com>
2018-03-08 14:03:38 +0100

    Removed ClosedSandboxAction and implemented handling of RequestGameboardAction

--- controller/src/main/java/com/datx02_18_35/controller/Controller.java
+++ controller/src/main/java/com/datx02_18_35/controller/Controller.java
@@ -3,13 +3,12 @@ package com.datx02_18_35.controller;
 import com.datx02_18_35.controller.dispatch.UnhandledActionException;
 import com.datx02_18_35.controller.dispatch.actions.Action;
 import com.datx02_18_35.controller.dispatch.ActionConsumer;
-import com.datx02_18_35.controller.dispatch.actions.ClosedSandboxAction;
+import com.datx02_18_35.controller.dispatch.actions.RefreshGameboardAction;
 import com.datx02_18_35.controller.dispatch.actions.RefreshInventoryAction;
+import com.datx02_18_35.controller.dispatch.actions.RequestGameboardAction;
 import com.datx02_18_35.controller.dispatch.actions.RequestInventoryAction;
 import com.datx02_18_35.model.game.Level;
 import com.datx02_18_35.model.game.Session;
-
-import javax.naming.ldap.Control;
 
 
 /**
@@ -30,9 +29,12 @@ public class Controller extends ActionConsumer {
             Action reply = new RefreshInventoryAction(session.getAssumptions(), session.getInventorys());
             action.callback(reply);
         }
-        else if (action instanceof ClosedSandboxAction) {
+        else if (action instanceof RequestGameboardAction) {
             assert session != null;
+            Action reply = new RefreshGameboardAction(session.getGameBoard());
+            action.callback(reply);
         }
+        
         else {
             throw new UnhandledActionException(action);
         }
