2e669ccfb6d2f9938263b55880c15f73d637500e
Johannes(raxxor) <johannes9533@hotmail.com>
2018-03-18 21:53:52 +0100

    added levels intent

---
+++ app/src/main/java/com/datx02_18_35/android/Levels.java
@@ -0,0 +1,15 @@
+package com.datx02_18_35.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import game.logic_game.R;
+
+public class Levels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_levels);
+    }
+}
