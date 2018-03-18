acfd24cf3ef9b21a6d576d75453ca823de3e6105
Johannes(raxxor) <johannes9533@hotmail.com>
2018-03-18 22:14:43 +0100

    levels activity + adapter skeletion

--- app/src/main/java/com/datx02_18_35/android/Levels.java
+++ app/src/main/java/com/datx02_18_35/android/Levels.java
@@ -2,14 +2,37 @@ package com.datx02_18_35.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 import game.logic_game.R;
 
 public class Levels extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    GridLayoutManager gridLayoutManager;
+    LevelsAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_levels);
+
+        recyclerView = (RecyclerView) findViewById(R.id.levels_recyclerview);
+
+
+        //dynamic span count later
+        gridLayoutManager = new GridLayoutManager(getApplication(), 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        //add levels
+
+
+        adapter = new LevelsAdapter(new ArrayList<>(),this);
+        recyclerView.setAdapter(adapter);
+
+
     }
 }
