3868354fbd4cbda82951f3c2bb1d997ceec95653
johannes(raxxor) <johannes9533@hotmail.com>
2018-04-02 17:40:57 +0200

    gravity not working correctly, try relativelayout

--- app/src/main/java/com/datx02_18_35/android/CardFactory.java
+++ app/src/main/java/com/datx02_18_35/android/CardFactory.java
@@ -1,14 +1,20 @@
 package com.datx02_18_35.android;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.view.GravityCompat;
 import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.datx02_18_35.model.expression.Absurdity;
 import com.datx02_18_35.model.expression.Expression;
+import com.datx02_18_35.model.expression.Operator;
 import com.datx02_18_35.model.expression.Proposition;
 
 import java.util.Map;
@@ -16,6 +22,10 @@ import java.util.Map;
 import game.logic_game.R;
 
 public class CardFactory {
+
+
+    //card 1 / 5 ratio
+
 
     private static final int cardElevation = 20; //dp units
     private static final int cardMargins = Math.round(Tools.convertDpToPixel((float)5));
@@ -56,6 +66,43 @@ public class CardFactory {
             card.addView(imageView);
             cardView.addView(card);
             sSymbol(expr,imageView,symbolMap);
+            return;
+        }
+
+        //expand expression
+        Operator operator = (Operator) expr;
+        Expression op1 = operator.getOperand1();
+        Expression op2 = operator.getOperand1();
+
+        // 1 operator 2 operands
+        if( (op1 instanceof Proposition | op1 instanceof Absurdity) &  (op2 instanceof Proposition | op2 instanceof Absurdity) ){
+
+
+            //try to create relative layout first
+            RelativeLayout relativeLayout = new RelativeLayout(cardView.getContext());
+            cardView.addView(relativeLayout);
+
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,(2*cardView.getLayoutParams().height)/5);
+
+            lp.gravity= Gravity.END;
+
+            //upper card
+            CardView cardView1 = new CardView(cardView.getContext());
+            cardView1.setLayoutParams(lp);
+            cardView1.setBackgroundColor(Color.BLACK);
+
+            //lower card
+            CardView cardView2 = new CardView(cardView.getContext());
+            cardView2.setLayoutParams(lp);
+            cardView2.setBackgroundColor(Color.BLUE);
+
+
+            //middle operator
+            ImageView imageView = new ImageView(cardView.getContext());
+
+
+            cardView.addView(cardView1);
+            cardView.addView(cardView2);
         }
 
     }
