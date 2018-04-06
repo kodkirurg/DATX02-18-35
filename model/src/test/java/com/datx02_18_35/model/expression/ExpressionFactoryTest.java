3d2056d783f0ab8cc7b5160db97df016000209bf
Johannes(raxxor) <johannes9533@hotmail.com>
2018-04-07 01:10:29 +0200

    empty unity test

---
+++ model/src/test/java/com/datx02_18_35/model/expression/ExpressionFactoryTest.java
@@ -0,0 +1,36 @@
+package com.datx02_18_35.model.expression;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExpressionFactoryTest {
+
+    @Test
+    public void getSymbolMap() {
+    }
+
+    @Test
+    public void createProposition() {
+    }
+
+    @Test
+    public void createPropositionWithMap() {
+    }
+
+    @Test
+    public void createAbsurdity() {
+    }
+
+    @Test
+    public void createNegation() {
+    }
+
+    @Test
+    public void createOperator() {
+    }
+
+    @Test
+    public void applyRule() {
+    }
+}
