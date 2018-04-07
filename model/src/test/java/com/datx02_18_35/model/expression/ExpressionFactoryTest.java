dc23beaa00fee80f16fea9d77b0c2ce25b68d5d2
Johannes(raxxor) <johannes9533@hotmail.com>
2018-04-07 02:00:21 +0200

    simple tests for a few of ExpressionFactory methods

--- model/src/test/java/com/datx02_18_35/model/expression/ExpressionFactoryTest.java
+++ model/src/test/java/com/datx02_18_35/model/expression/ExpressionFactoryTest.java
@@ -6,12 +6,22 @@ import static org.junit.Assert.*;
 
 public class ExpressionFactoryTest {
 
+    final static String propositionAsString1 = "abc123ABC&#";
+    final static String propositionAsString2 = "oasd93Äå";
+    final static ExpressionFactory expressionFactory = new ExpressionFactory(null);
+
     @Test
     public void getSymbolMap() {
     }
 
     @Test
     public void createProposition() {
+        Proposition proposition = expressionFactory.createProposition(propositionAsString1);
+
+        assertTrue(proposition != null);
+        assertTrue(proposition.toString().equals( "(" + propositionAsString1 +")" ));
+        assertTrue (proposition.getSymbol()==null);
+        assertTrue(proposition.calculateHash() != 0);
     }
 
     @Test
@@ -20,10 +30,23 @@ public class ExpressionFactoryTest {
 
     @Test
     public void createAbsurdity() {
+        Absurdity absurdity = expressionFactory.createAbsurdity();
+
+        assertTrue( absurdity != null);
+        assertTrue(absurdity.toString().equals("(#)"));
     }
 
     @Test
     public void createNegation() {
+        Expression negation = expressionFactory.createNegation(expressionFactory.createProposition(propositionAsString1));
+        Expression proposition = expressionFactory.createProposition(propositionAsString1);
+
+
+        //assert simple propsition is negated
+        assert negation instanceof Negation;
+        assert negation.toString().equals("(!" + proposition.toString() + ")");
+        assert negation.calculateHash() != 0;
+        
     }
 
     @Test
