d822c7765e5c7b14a4729a31aa5233e17ce74ebd
Robin Åstedt <robin.astedt@gmail.com>
2018-04-10 16:30:03 +0200

    Added debug option to unlock all categories

--- model/src/main/java/com/datx02_18_35/model/userdata/UserData.java
+++ model/src/main/java/com/datx02_18_35/model/userdata/UserData.java
@@ -37,6 +37,8 @@ public class UserData implements Serializable {
         }
         LevelCategory firstCategory = levelCollection.getCategories().get(0);
         categoryProgressionMap.put(firstCategory, new LevelCategoryProgression(LevelCategoryProgression.Status.UNLOCKED));
+
+        checkDebugSettings();
     }
 
     public static byte[] saveUserData(UserData userData) {
@@ -97,10 +99,17 @@ public class UserData implements Serializable {
                 }
             }
         }
-
+        userData.checkDebugSettings();
         return userData;
     }
 
+    private void checkDebugSettings() {
+        if (Config.DEBUG_UNLOCK_ALL) {
+            for (LevelCategory category : categoryProgressionMap.keySet()) {
+                categoryProgressionMap.put(category, new LevelCategoryProgression(LevelCategoryProgression.Status.UNLOCKED));
+            }
+        }
+    }
 
     public void logCategoryProgression() {
         for (Map.Entry<LevelCategory, LevelCategoryProgression> entry : categoryProgressionMap.entrySet()) {
