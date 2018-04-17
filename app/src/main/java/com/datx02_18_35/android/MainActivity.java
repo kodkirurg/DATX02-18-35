85d9e4ea599de6abf3cbc852888a44d197f71565
Robin Åstedt <robin.astedt@gmail.com>
2018-04-17 15:52:12 +0200

    Fixed comment typo

--- app/src/main/java/com/datx02_18_35/android/MainActivity.java
+++ app/src/main/java/com/datx02_18_35/android/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Init model
+        // Init controller & model
         initController();
 
         //Add listener
